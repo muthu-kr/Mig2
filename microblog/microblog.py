f1855d3b7dd2a94d788bb3c3e936f6c2a30874b9
muthu <coderbro@hotmail.com>
2018-04-22 19:16:50 +0530

    Done with ex4 Databases

--- microblog/microblog.py
+++ microblog/microblog.py
@@ -1,1 +1,6 @@
-from app1 import app
+from app1 import app, db
+from app1.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+	return {'db': db, 'User': User, 'Post': Post}
